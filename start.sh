a14c503b8d0157e4c2e6bb83501daf808421dde7
Flavien PERIER <flavien.perier@pm.me>
2025-11-15 22:19:34 +0100

    Update proxy env

--- start.sh
+++ start.sh
@@ -4,9 +4,7 @@ set -e
 
 chown -R squid:squid /var/squid
 
-DNS_IP=`ping $DNS -c 1 | head -n 1 | cut -f 2 -d "(" | cut -f 1 -d ")"`
-
-sed -i s/"^dns_nameservers dns$"/"dns_nameservers $DNS_IP"/g \
+sed -i s/"^dns_nameservers dns$"/"dns_nameservers $DNS"/g \
     /etc/squid/squid.conf
 
 squid -N -f /etc/squid/squid.conf -z
